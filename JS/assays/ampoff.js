a7ff8ec0a03b0deee378bd1428af2630b1e1efbb
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2024-09-10 10:38:45 -0700

    rename gaba.js and synthetics.js

---
+++ JS/assays/ampoff.js
@@ -0,0 +1,9 @@
+export const AMPoff = {
+  defaultEstablishedMeans: {
+    'Amphetamine 1': { peakArea: 1000, retentionTime: { min: 2, max: 5 } }
+  },
+  defaultIstdAnalytes: {
+    'IS_Amphetamine_D5': { peakArea: 1000, retentionTime: { min: 2, max: 5 } }
+  },
+  instruments: ['LCMS 3', 'LCMS 7']
+};
